2a0b557eaf94b27bac50e733a9383e673ca2c797
Nilgül Çakır <75910659+nilgulcakir@users.noreply.github.com>
2024-03-29 20:40:01 +0300

    Initial Commit

---
+++ NetflixAppMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetflixAppMVVM
+//
+//  Created by Nilgul Cakir on 29.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
